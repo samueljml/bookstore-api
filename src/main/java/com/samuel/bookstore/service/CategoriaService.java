31a06518c48aeb3a279603cdebe224a963b04120
Samuel <samuel.andrade@fatec.sp.gov.br>
2021-02-07 14:56:35 -0300

    exceção personalizada para categoria não encontrada

--- src/main/java/com/samuel/bookstore/service/CategoriaService.java
+++ src/main/java/com/samuel/bookstore/service/CategoriaService.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.samuel.bookstore.domain.Categoria;
 import com.samuel.bookstore.repositories.CategoriaRepository;
+import com.samuel.bookstore.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
